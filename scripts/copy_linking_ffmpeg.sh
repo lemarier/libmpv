820d18c4884f7d04013ed4dcdfc3861741aea457
David L <david@lemarier.ca>
2020-06-01 06:51:54 -0400

    New static lib

---
+++ scripts/copy_linking_ffmpeg.sh
@@ -0,0 +1,34 @@
+#determine the last local ffmpeg version
+COPY_FROM_PATH=""
+for ffmpeg_path in `brew info ffmpeg | grep 'Cellar/ffmpeg/.*\*' | awk -F' ' '{ print $1 }'`; do COPY_FROM_PATH="$(echo $ffmpeg_path)"; done;
+
+#copy all the binaries
+for source in `find $COPY_FROM_PATH/bin -type f | grep ffmpeg`; do cp -v -f $source ./mac; done;
+
+#I need ffprobe and ffmpeg only, so I remove ffserver here. If you need it - comment this line
+cd mac && rm -f ffserver && cd ..
+
+#replace references to an absolute path with @executable_path
+#which will allow to run ffmpeg that loads dylibs from the bundle
+function replace_dlybs() {
+	DYLIBS=`otool -L $1 | grep "/usr/local/Cellar" | awk -F' ' '{print \$1 }'`
+	for dylib in $DYLIBS; do sudo install_name_tool -change $dylib @executable_path/`basename $dylib` $1; done;
+	for dylib in $DYLIBS; do cp -f -n $dylib ./mac; done;
+	DYLIBS=`otool -L $1 | grep "/usr/local/opt" | awk -F' ' '{print \$1 }'`
+	for dylib in $DYLIBS; do sudo install_name_tool -change $dylib @executable_path/`basename $dylib` $1; done;
+	for dylib in $DYLIBS; do cp -f -n $dylib ./mac; done;
+
+	sudo install_name_tool -id @executable_path/`basename $dylib` $1
+}
+
+#first replace occurences in ffmpeg and ffprobe binaries
+for file in `ls mac/*`; do
+	echo replacing dylibs for $file
+	replace_dlybs $file
+done;
+
+#then replace occurrences in all the additional dylibs that were copied in the previous step
+for file in `ls mac/*.dylib`; do
+	echo replacing dylibs for $file
+	replace_dlybs $file
+done;
---
+++ scripts/copy_linking_mpv.sh
@@ -0,0 +1,36 @@
+#determine the last local ffmpeg version
+COPY_FROM_PATH=""
+for ffmpeg_path in `brew info mpv | grep 'Cellar/mpv/.*\*' | awk -F' ' '{ print $1 }'`; do COPY_FROM_PATH="$(echo $ffmpeg_path)"; done;
+
+# get mpv lib
+for source in `find $COPY_FROM_PATH/lib -type f | grep libmpv`; do cp -v -f $source ./mac; done;
+
+
+#replace references to an absolute path with @executable_path
+#which will allow to run ffmpeg that loads dylibs from the bundle
+function replace_dlybs() {
+	DYLIBS=`otool -L $1 | grep "/usr/local/Cellar" | awk -F' ' '{print \$1 }'`
+	for dylib in $DYLIBS; do sudo install_name_tool -change $dylib @executable_path/`basename $dylib` $1; done;
+	for dylib in $DYLIBS; do cp -f -n $dylib ./mac; done;
+	DYLIBS=`otool -L $1 | grep "/usr/local/opt" | awk -F' ' '{print \$1 }'`
+	for dylib in $DYLIBS; do sudo install_name_tool -change $dylib @executable_path/`basename $dylib` $1; done;
+	for dylib in $DYLIBS; do cp -f -n $dylib ./mac; done;
+	sudo install_name_tool -id @executable_path/`basename $dylib` $1
+}
+
+#first replace occurences in ffmpeg and ffprobe binaries
+for file in `ls mac/*`; do
+	echo replacing dylibs for $file
+	replace_dlybs $file
+done;
+
+#then replace occurrences in all the additional dylibs that were copied in the previous step
+for file in `ls mac/*.dylib`; do
+	echo replacing dylibs for $file
+	replace_dlybs $file
+done;
+
+rm -Rf mac/Python
+rm -Rf mac/libmpv.1.dylib
+ln -s ./mac/libmpv.1.107.0.dylib ./mac/libmpv.1.dylib
+ln -s ./mac/libmpv.1.107.0.dylib ./mac/libmpv.dylib
